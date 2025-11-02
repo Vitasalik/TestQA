208a7991367befdc4f2f7ee6da40965a79c0cc4e
Vitasalik <vitalya_kovalevs@mail.ru>
2025-11-02 13:28:26 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,16 @@ public class Main {
         var tm = new Print();
 
         tm.PrintText("Hi");
+        tm.PrintInt(865);
     }
 
     public static class Print{
 
         public void PrintText(String text){
+            System.out.println(text);
+        }
+
+        public void PrintInt(int text){
             System.out.println(text);
         }
     }
